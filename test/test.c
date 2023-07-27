15029d7ccfff274e55c4a187cf41785fd53aed3e
Vladimir Pleskonjic <21056458+vplesko@users.noreply.github.com>
2023-07-27 19:21:03 +0100

    Print code location when a test fails

--- test/test.c
+++ test/test.c
@@ -1,7 +1,15 @@
+#include <stdio.h>
+
 #include "unargs.h"
 
+#define PRINT_TEST_FAIL() \
+    fprintf(stderr, "Test failed: %s:%d %s\n", __FILE__, __LINE__, __func__)
+
 int testBasic(void) {
-    if (unargs_parse() < 0) return -1;
+    if (unargs_parse() < 0) {
+        PRINT_TEST_FAIL();
+        return -1;
+    }
 
     return 0;
 }
