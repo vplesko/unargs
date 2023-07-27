f7b2a94a3069467fea7e1a21bd81653b724356ef
Vladimir Pleskonjic <21056458+vplesko@users.noreply.github.com>
2023-07-27 21:27:56 +0100

    Implement required options

--- test/test.c
+++ test/test.c
@@ -1,32 +1,131 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "unargs.h"
 
 #define PRINT_TEST_FAIL() \
     fprintf(stderr, "Test failed: %s:%d %s\n", __FILE__, __LINE__, __func__)
 
+#define EXPECT_EQ(a, b) \
+    do { \
+        if ((a) != (b)) { \
+            PRINT_TEST_FAIL(); \
+            return -1; \
+        } \
+    } while (0)
+
+#define EXPECT_STR_EQ(a, b) \
+    do { \
+        if (strcmp(a, b) != 0) { \
+            PRINT_TEST_FAIL(); \
+            return -1; \
+        } \
+    } while (0)
+
 int testBasic(void) {
     char *argv[] = {
         "main",
+        "-i", "123",
+        "-str", "abc",
     };
-    int argc = sizeof(argv) / sizeof(*argv);
+
+    int i;
+    const char *str;
 
     unargs_Param params[] = {
-        unargs_int("i", NULL),
-        unargs_string("str", NULL),
+        unargs_int("i", &i),
+        unargs_string("str", &str),
     };
 
-    if (unargs_parse(argc, argv,
+    if (unargs_parse(
+            sizeof(argv) / sizeof(*argv), argv,
             sizeof(params) / sizeof(*params), params) < 0) {
         PRINT_TEST_FAIL();
         return -1;
+    }
+
+    EXPECT_EQ(i, 123);
+    EXPECT_STR_EQ(str, "abc");
+
+    return 0;
+}
+
+int testBadArgs(void) {
+    {
+        char *argv[] = {
+            "main",
+            "-i", "xyz",
+        };
+
+        unargs_Param params[] = {
+            unargs_int("i", NULL),
+        };
+
+        if (unargs_parse(
+                sizeof(argv) / sizeof(*argv), argv,
+                sizeof(params) / sizeof(*params), params) >= 0) {
+            PRINT_TEST_FAIL();
+            return -1;
+        }
+    }
+    {
+        char *argv[] = {
+            "main",
+            "-i", "9223372036854775807",
+        };
+
+        unargs_Param params[] = {
+            unargs_int("i", NULL),
+        };
+
+        if (unargs_parse(
+                sizeof(argv) / sizeof(*argv), argv,
+                sizeof(params) / sizeof(*params), params) >= 0) {
+            PRINT_TEST_FAIL();
+            return -1;
+        }
+    }
+    {
+        char *argv[] = {
+            "main",
+            "-i", "123",
+            "-i", "123",
+        };
+
+        unargs_Param params[] = {
+            unargs_int("i", NULL),
+        };
+
+        if (unargs_parse(
+                sizeof(argv) / sizeof(*argv), argv,
+                sizeof(params) / sizeof(*params), params) >= 0) {
+            PRINT_TEST_FAIL();
+            return -1;
+        }
+    }
+    {
+        char *argv[] = {
+            "main",
+        };
+
+        unargs_Param params[] = {
+            unargs_int("i", NULL),
+        };
+
+        if (unargs_parse(
+                sizeof(argv) / sizeof(*argv), argv,
+                sizeof(params) / sizeof(*params), params) >= 0) {
+            PRINT_TEST_FAIL();
+            return -1;
+        }
     }
 
     return 0;
 }
 
 int main(void) {
-    if (testBasic() != 0) {
+    if (testBasic() != 0 ||
+        testBadArgs() != 0) {
         return -1;
     }
 
--- unargs.h
+++ unargs.h
@@ -1,5 +1,7 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 enum unargs__Type {
@@ -63,11 +65,71 @@ int unargs__verifyArgs(int argc, char * const *argv) {
     return 0;
 }
 
+const char* unargs__optName(const char *arg) {
+    return arg + 1;
+}
+
+int unargs__parseVal(const char *arg, const unargs_Param *param) {
+    if (param->_type == unargs__TypeInt) {
+        long l;
+        char *end;
+        l = strtol(arg, &end, 0);
+        if (*end != '\0') {
+            fprintf(stderr, "@TODO\n");
+            return -1;
+        }
+
+        int i = (int)l;
+        if (i != l) {
+            fprintf(stderr, "@TODO\n");
+            return -1;
+        }
+
+        if (param->_dst != NULL) *(int*)param->_dst = i;
+    } else if (param->_type == unargs__TypeString) {
+        if (param->_dst != NULL) *(const char**)param->_dst = arg;
+    } else {
+        assert(false);
+    }
+
+    return 0;
+}
+
+int unargs__parseOpts(
+    int argc, char * const *argv,
+    size_t len, const unargs_Param *params) {
+    for (size_t p = 0; p < len; ++p) {
+        bool found = false;
+
+        for (int a = 1; a < argc; a += 2) {
+            if (strcmp(params[p]._name, unargs__optName(argv[a])) == 0) {
+                if (found) {
+                    fprintf(stderr, "@TODO\n");
+                    return -1;
+                }
+
+                if (unargs__parseVal(argv[a + 1], &params[p]) < 0) return -1;
+
+                found = true;
+            }
+        }
+
+        if (!found) {
+            fprintf(stderr, "@TODO\n");
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
 int unargs_parse(
     int argc, char * const *argv,
     size_t len, const unargs_Param *params) {
     unargs__verifyParams(len, params);
     unargs__verifyArgs(argc, argv);
+
+    if (unargs__parseOpts(argc, argv, len, params) < 0) return -1;
 
     return 0;
 }
